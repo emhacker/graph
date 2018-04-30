d21656506118a49fcb5ef28105b523fa1a495637
elior <elior.malul@appsflyer.com>
2018-04-30 14:07:43 +0300

    We can now build graphs

--- src/graph/core.clj
+++ src/graph/core.clj
@@ -1,6 +1,64 @@
-(ns graph.core)
+(ns graph.core
+  (:require [clojure.tools.logging :as log]))
 
+;;
+;; Graph utilites.
+;;
+(defn _add-graph-set
+  [graph k new-elem]
+  (assoc graph k (-> (get graph k) (conj ,,, new-elem))))
+
+(defn _valid-edge?
+  "Indicates whether the given edge is valid (IFF the nodes it references exists
+  in the graph's nodes)."
+  [graph e]
+  (log/debugf "Validating edge %s for graph %s"e graph)
+  (every? #(some #{%} (get graph :nodes)) (vals e)))
+
+(defn _add-edge
+    "Internal function - add-edge; returns the modified graph when successful;
+    when unsuccessful, invokes the given error function."
+    [graph new-edge error-fn]
+    (if (_valid-edge? graph new-edge)
+      (_add-graph-set graph :edges new-edge)
+      (error-fn)))
+
+;;
+;; Graph API.
+;;
 (defn create-graph
   "Creates an empty graph."
   []
   {:nodes '() :edges '()})
+
+(defn nodes
+  "Gets the nodes associated to the given graph."
+  [graph]
+  (log/debugf "Retrieving nodes for ")
+  (get graph :nodes))
+
+(defn edges
+  "Gets the edges associated to the given graph."
+  [graph]
+  (get graph :edges))
+
+(defn add-node
+  "Adds a node to the given graph."
+  [graph new-node]
+  (if (some #{new-node} (nodes graph))
+    (graph)
+    (_add-graph-set graph :nodes new-node)))
+
+(defn add-edge
+  "Adds the given edge to the graph. (The fact that the edge references existing
+  nodes is asserted"
+  ([graph new-edge]
+    (_add-edge
+      graph
+      new-edge
+      #(throw (Exception. (str "Invalid node" new-edge)))))
+  ([graph new-edge error-fn]
+   (_add-edge
+     graph
+     new-edge
+     error-fn)))
--- test/graph/core_test.clj
+++ test/graph/core_test.clj
@@ -4,4 +4,14 @@
 
 (deftest a-test
   (testing "Graph creation"
-    (is (not (nil? create-graph)))))
+    (is (not (nil? create-graph))))
+  (testing "Node addition"
+    (is (= [:n1] (-> (create-graph) (add-node ,,, :n1) (nodes ,,,)))))
+  (testing "Edge addition"
+    (let [e1 {:src :n1 :dst :n2}]
+      (is (= [e1]
+            (-> (create-graph)
+                (add-node ,,, :n1)
+                (add-node ,,, :n2)
+                (add-edge ,,, e1)
+                (edges ,,,)))))))
